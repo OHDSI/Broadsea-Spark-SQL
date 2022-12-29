306c221c866d0b8600de6a468997031a85fc4904
leeevans <levans@ltscomputingllc.com>
2022-12-29 13:37:26 -0500

    improve example R script

--- test_spark_sql.R
+++ test_spark_sql.R
@@ -1,26 +1,39 @@
 library(DatabaseConnector)
 
 # call this function one time to download the Spark jdbc driver
-DatabaseConnector::downloadJdbcDrivers(dbms='spark', pathToDriver='~/jdbcdrivers') 
+# DatabaseConnector::downloadJdbcDrivers(dbms='spark', pathToDriver='~/jdbcdrivers')
 
 # connect to Spark thrift server running inside Broadsea-Spark-SQL Docker container
 conn <- connect(dbms='spark',
                 user = 'secret-userid',
                 password = 'secret-password',
-                connectionString='jdbc:spark://0.0.0.0:10000;')
+                connectionString='jdbc:spark://0.0.0.0:10000;UseNativeQuery=1')
 
 dbGetQuery(conn, 'show databases;')
-
-executeSql(conn, 'drop table if exists my_table;')
+dbGetQuery(conn, 'show tables;')
 
 data <- data.frame(x = c(1, 2, 3), y = c("a", "b", "c"))
-insertTable(conn, "default", "my_table", data)
+
+# use try() to handle the error message from Spark Simba JDBC driver when calling insertTable():
+# java.sql.SQLException: [Simba][JDBC](10040) Cannot use commit while Connection is in auto-commit mode.
+try(insertTable(conn, "default", "my_table", data))
+dbGetQuery(conn, 'show tables;')
 
 dbGetQuery(conn, 'select * from my_table;')
 # SQL query output:
 # x y
-# 1 2 b
-# 2 3 c
-# 3 1 a
+# 2 b
+# 3 c
+# 1 a
+
+executeSql(conn, 'delete from my_table where x = 3;')
+dbGetQuery(conn, 'select * from my_table;')
+# SQL query output:
+# x y
+# 1 a
+# 2 b
+
+executeSql(conn, 'drop table if exists my_table;')
+dbGetQuery(conn, 'show tables;')
 
 disconnect(conn)
